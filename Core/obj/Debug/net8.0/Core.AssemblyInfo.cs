08f19927c4c4e6f99213f8bb2969a627d463021f
nikolanovosel <nikolanovosel@yahoo.com>
2025-01-31 00:35:34 +0100

    set to 5 sec implicit wait

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d940150fc7c6ae486e092684040104d2ad052cca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e177d272ce2d7f959935be4ab1861cf9aa8a7441")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
